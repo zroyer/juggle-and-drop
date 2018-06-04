99b87ab450c7bc4117541bb32d6c705ce1955a17
zroyer <zroyer@utexas.edu>
2018-06-04 15:24:17 -0500

    styled-components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,28 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const titleColor = '#FFFFFF';
+const wrapperColor = '#1BAAA3'
+
+const Wrapper = styled.div`
+  background-color: ${props => props.wrapperColor ||  'grey'};
+  color: ${props => props.titleColor ||  'white'};
+  text-align: center;
+  padding: 12px;
+  font-weight: 500;
+  font-size: 1.15rem;
+`
 
 class Header extends Component {
-  render = () => (
-    <header
-      style={{
-        background: "#1BAAA3",
-        padding: 8,
-        textAlign: "center",
-        color: "#FFFFFF",
-        fontWeight: 500,
-        fontSize: "1.15rem"
-      }}>
-      <div>Doing Things</div>
-    </header>
-  );
+  render() {
+    return (
+      <Wrapper
+        wrapperColor={wrapperColor}
+        titleColor={titleColor}>
+          Doing Things
+      </Wrapper>
+    );
+  }
 }
 
 export default Header;
